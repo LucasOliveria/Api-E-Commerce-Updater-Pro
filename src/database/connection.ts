92268deaf0bdaf9dd45bdbd8d3b0a6a00fdb3e2d
Lucas Oliveira <lucasoliveiralog@hotmail.com>
2023-09-18 20:15:27 -0300

    Deploy Branch

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -7,6 +7,7 @@ export const knex = connection({
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: { rejectUnauthorized: false }
   }
 });
